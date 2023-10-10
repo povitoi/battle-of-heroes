c7b5b8e225792907bc9c21b504adb3eface4a2ed
povitoi <145909028+povitoi@users.noreply.github.com>
2023-10-10 13:29:57 +0200

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,9 +1,3 @@
-import java.io.BufferedReader;
-import java.io.Console;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class Hero extends Creature {
 
     public static Hero hero;
