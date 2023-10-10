f65acef9b13e06f659b73ac27400bffd63b320f4
povitoi <145909028+povitoi@users.noreply.github.com>
2023-10-10 13:29:28 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,6 @@
-import javax.print.event.PrintJobAttributeListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
